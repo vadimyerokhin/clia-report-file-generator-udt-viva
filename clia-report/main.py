cdfe925dc5f6b1726535471d1c1bc4e0f53228ce
Vadim Yerokhin <vadim.yerokhin@okstate.edu>
2025-10-17 19:33:27 -0500

    added uv deps

---
+++ clia-report/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from clia-report!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from clia-report-file-generator-udt-viva!")
+
+
+if __name__ == "__main__":
+    main()
